92873c958efea20a68a3f95baa6a2308dbb1df39
Shawn B <36942857+easyrider22@users.noreply.github.com>
2021-06-14 04:16:13 -0700

    Add files via upload

--- LoadImageWindow.xaml.cs
+++ LoadImageWindow.xaml.cs
@@ -1,16 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -22,46 +16,66 @@ namespace BLOOMINGTREES_WpfApp
     public partial class LoadImageWindow : Window
     {
         string displayimg, filePath, location;
-        
+
         public LoadImageWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void editImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Select a picture";
+            open.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+            "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+            "Portable Network Graphic (*.png)|*.png";
+
+            // display image in Image Tag
+            if (open.ShowDialog() == true)
+            {
+                //displayimg = open.SafeFileName;
+                
+                location = open.FileName;
+                Process msPaint = new();
+                msPaint.StartInfo.FileName = "mspaint.exe";
+                //msPaint.StartInfo.Arguments = location; // if you need some arguments to open
+                msPaint.Start();
+                displayImage.Source = null;
+            }
         }
 
         public BitmapImage Source { get; internal set; }
 
         private void selectImageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Select a picture";
+            open.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+            "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+            "Portable Network Graphic (*.png)|*.png";
+
+            // display image in Image Tag
+            if (open.ShowDialog() == true)
             {
+                MainWindow thisImage = new MainWindow();
+                displayimg = open.SafeFileName;
+                //following will display image inside UI Image tag = "displayImage".
+                displayImage.Source = new BitmapImage(new Uri(open.FileName));
+                thisImage.uxAddImagePath.IsEnabled = true;
+                thisImage.uxAddImagePath.Text = displayimg;
+                //thisImage.uxAddImagePath.Text = open.FileName;                
+                BindingExpression be = thisImage.uxAddImagePath.GetBindingExpression(TextBox.TextProperty);
+                be.UpdateSource();
+                filePath = open.FileName;
+                location = open.FileName;               
 
 
-                OpenFileDialog open = new OpenFileDialog();
-                open.Title = "Select a picture";
-                open.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
-                  "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
-                  "Portable Network Graphic (*.png)|*.png";
+            }//end of if statement
 
-
-                if (open.ShowDialog() == true)
-                {
-
-
-                    MainWindow thisImage = new MainWindow();
-                    displayimg = open.SafeFileName;
-                    //following will display image inside UI Image tag = "displayImage".
-                    displayImage.Source = new BitmapImage(new Uri(open.FileName));
-                    //thisImage.uxAddImagePath.IsEnabled = true;
-                    thisImage.uxAddImagePath.Text = displayimg;
-
-                    //thisImage.uxAddImagePath.Text = open.FileName;
-
-                    // following two lines will add image's path text to Textbox = "uxAddImagePath" in MainWindow but does not work here - only works from MainWindow event ha
-                    BindingExpression be = thisImage.uxAddImagePath.GetBindingExpression(TextBox.TextProperty);
-                    be.UpdateSource();
-                    filePath = open.FileName;
-                    location = open.FileName;
-                }//end of if statement
-
-            } //end of Event Handler selectImageButton    
+        } //end of Event Handler selectImageButton                   
 
     }//end of Partial Class LoadImageWindow : Window
 
@@ -80,4 +94,5 @@ namespace BLOOMINGTREES_WpfApp
 
 
 
-       
+
+
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.EntityFrameworkCore;
-
+using System.Diagnostics;
 
 namespace BLOOMINGTREES_WpfApp
 {
@@ -26,17 +26,13 @@ namespace BLOOMINGTREES_WpfApp
     {
 
         ImagesFlowers NewFlowers = new ImagesFlowers();
-        string displayimg, filePath, location;        
+        string displayimg, filePath, location;
+
+        string selection, selectedValue;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            var flowers = new List<Models.BloomingTrees>();
-
-            flowers.Add(new Models.BloomingTrees { ID = 1, ProductID = "FPB566", Name = "Acacia", Description = "Golden Yellow tree", Path = "E:\\'Users", Quantity = 13, Price = 12.77});
-            flowers.Add(new Models.BloomingTrees { ID = 2, ProductID = "FPS212", Name = "Amaranth", Description = "Reddish Purple plant", Path = "E:\\Users", Quantity = 1, Price = 2.00 });
-            flowers.Add(new Models.BloomingTrees { ID = 3, ProductID = "FPB417", Name = "Primrose", Description = "dazzling multi-colored flowers", Path = "E:\\'Users", Quantity = 27, Price = 7.99 });
 
             var bloom = new BloomingTreesContext();
             bloom.ImagesFlowers.Load();
@@ -44,7 +40,6 @@ namespace BLOOMINGTREES_WpfApp
             uxRecordCount.Text = "Record Count: " + recordCount.ToString();
             GetFlowers();
             uxAddNewItemGrid.DataContext = NewFlowers;
-
         }
 
         private void GetFlowers()
@@ -54,8 +49,9 @@ namespace BLOOMINGTREES_WpfApp
             FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection();
         }
 
-        private void AddFlowers(object sender, RoutedEventArgs e)
+        private void Save_AddFlowers(object sender, ExecutedRoutedEventArgs e)
         {
+            //My code to Add New Flowers
             var flowerAdd = new BloomingTreesContext();
             flowerAdd.ImagesFlowers.Add(NewFlowers);
             flowerAdd.SaveChanges();
@@ -65,7 +61,33 @@ namespace BLOOMINGTREES_WpfApp
             var bloomCount = new BloomingTreesContext();
             int recordCount = (int)bloomCount.ImagesFlowers.Count();
             uxRecordCount.Text = "Record Count: " + recordCount.ToString();
+            displayImage.Source = null;
         }
+
+
+        //Verify that all fields in Add New Flowers are populated except purchase date (allows Nulls)
+        private void Save_CanAddFlowers(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !(string.IsNullOrEmpty(uxAddProductId.Text) || string.IsNullOrEmpty(uxAddName.Text) ||
+                string.IsNullOrEmpty(uxAddDescription.Text) || string.IsNullOrEmpty(uxAddImagePath.Text) ||
+                string.IsNullOrEmpty(uxAddQty.Text) || string.IsNullOrEmpty(uxAddrice.Text) ||
+                string.IsNullOrEmpty(uxAddSupplier.Text));
+        }
+
+
+        //private void AddFlowers(object sender, RoutedEventArgs e)
+        //{
+        //    var flowerAdd = new BloomingTreesContext();
+        //    flowerAdd.ImagesFlowers.Add(NewFlowers);
+        //    flowerAdd.SaveChanges();
+        //    GetFlowers();
+        //    NewFlowers = new ImagesFlowers();            
+        //    uxAddNewItemGrid.DataContext = NewFlowers;
+        //    var bloomCount = new BloomingTreesContext();            
+        //    int recordCount = (int)bloomCount.ImagesFlowers.Count();
+        //    uxRecordCount.Text = "Record Count: " + recordCount.ToString();
+        //    displayImage.Source = null;
+        //}
 
 
         ImagesFlowers itemSelected = new ImagesFlowers();
@@ -77,13 +99,58 @@ namespace BLOOMINGTREES_WpfApp
             GetFlowers();
         }
 
-        private void UpdateItems(object sender, RoutedEventArgs e)
+        private void Save_UpdateFlowers(object sender, ExecutedRoutedEventArgs e)
         {
+            //My code to Update Flowers
             var flowerUpdate = new BloomingTreesContext();
             flowerUpdate.Update(itemSelected);
             flowerUpdate.SaveChanges();
             GetFlowers();
+            uxUpdateProductId.Text = null;
+            uxUpdateItemName.Text = null;
+            uxUpdateItemDescription.Text = null;
+            uxEditImagePath.Text = null;
+            uxUpdateItemQty.Text = null;
+            uxUpdateItemPrice.Text = null;
+            uxUpdateItemSupplier.Text = null;
+            uxUpdateDatePicker.Text = null;
+            displayImage.Source = null;
+            var bloomCount = new BloomingTreesContext();
+            int recordCount = (int)bloomCount.ImagesFlowers.Count();
+            uxRecordCount.Text = "Record Count: " + recordCount.ToString();
+            displayImage.Source = null;
         }
+
+        //Verify that all fields in Update Flowers are populated except purchase date (allows Nulls)
+        private void Save_CanUpdateFlowers(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !(string.IsNullOrEmpty(uxUpdateProductId.Text) || string.IsNullOrEmpty(uxUpdateItemName.Text) ||
+                string.IsNullOrEmpty(uxUpdateItemDescription.Text) || string.IsNullOrEmpty(uxEditImagePath.Text) ||
+                string.IsNullOrEmpty(uxUpdateItemQty.Text) || string.IsNullOrEmpty(uxUpdateItemPrice.Text) ||
+                string.IsNullOrEmpty(uxUpdateItemSupplier.Text));
+        }
+
+
+        //private void UpdateItems(object sender, RoutedEventArgs e)
+        //{
+        //    var flowerUpdate = new BloomingTreesContext();
+        //    flowerUpdate.Update(itemSelected);
+        //    flowerUpdate.SaveChanges();
+        //    GetFlowers();
+        //    uxUpdateProductId.Text = null;
+        //    uxUpdateItemName.Text = null;
+        //    uxUpdateItemDescription.Text = null;
+        //    uxEditImagePath.Text = null;
+        //    uxUpdateItemQty.Text = null;
+        //    uxUpdateItemPrice.Text = null;
+        //    uxUpdateItemSupplier.Text = null;
+        //    uxUpdateDatePicker.Text = null;
+        //    displayImage.Source = null;
+        //    var bloomCount = new BloomingTreesContext();
+        //    int recordCount = (int)bloomCount.ImagesFlowers.Count();
+        //    uxRecordCount.Text = "Record Count: " + recordCount.ToString();
+        //    displayImage.Source = null;
+        //}
 
         private void DeleteItem(object sender, RoutedEventArgs e)
         {
@@ -97,12 +164,28 @@ namespace BLOOMINGTREES_WpfApp
             uxRecordCount.Text = "Record Count: " + recordCount.ToString();
         }
 
-        private void OnNew_Click(object sender, RoutedEventArgs e)
+        private void OnNewPreview_Click(object sender, RoutedEventArgs e)
         {
             var window = new LoadImageWindow();
             Application.Current.MainWindow = window;
             //Close();
             window.Show();
+        }
+
+        private void OnNewQuery_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://www.bhg.com/gardening/plant-dictionary/";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            // To open url in Chrome, specify startInfo.File = "chrome.exe"
+            // To open url in FireFox specify firefox.exe
+            // To open url in Microsoft Edge specify msedge.exe
+            // To open url in default browser specify explorer.exe
+
+            startInfo.FileName = "explorer.exe";
+            startInfo.Arguments = url;
+            Process.Start(startInfo);
         }
 
         private void OnClose_Click(object sender, RoutedEventArgs e)
@@ -112,7 +195,46 @@ namespace BLOOMINGTREES_WpfApp
 
         private void QueryItems(object sender, RoutedEventArgs e)
         {
+            selection = this.CBSelectQueryField.SelectionBoxItem.ToString();
+            var flowerGet = new BloomingTreesContext();
+            flowerGet.ImagesFlowers.Load();
+            switch (selection)
+            {
 
+                case "ProductID":
+                    selectedValue = uxQueryProductId.Text.ToString();
+                    FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection().Where(x => x.ProductId.Contains(selectedValue));
+
+                    break;
+                case "ItemName":
+                    selectedValue = uxQueryItemName.Text.ToString();
+                    FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection().Where(x => x.ItemName.Contains(selectedValue));
+                    break;
+                case "Description":
+                    selectedValue = uxQueryItemDescription.Text.ToString();
+                    FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection().Where(x => x.ItemDescription.Contains(selectedValue));
+                    break;
+                case "Quantity":
+                    selectedValue = uxQueryItemQty.Text.ToString();
+                    FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection().Where(x => x.ItemQty.Equals(Int32.Parse(uxQueryItemQty.Text.ToString())));
+                    break;
+                case "Supplier":
+                    selectedValue = uxQueryItemSupplier.Text.ToString();
+                    FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection().Where(x => x.ItemSupplier.Contains(selectedValue));
+                    break;
+                case "Purchase Date":
+                    selectedValue = uxQueryPurchaseDate.Text.ToString();
+
+                    DateTime dateTimeSelected = Convert.ToDateTime(selectedValue);
+                    selectedValue = dateTimeSelected.ToString("M/d/yyyy hh:mm:ss");
+
+                    FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection().Where(x => x.ItemPurchaseDate.Contains(selectedValue));
+                    break;
+
+                default:
+                    break;
+
+            }
         }
 
 
@@ -125,94 +247,135 @@ namespace BLOOMINGTREES_WpfApp
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ComboBox cmb = sender as ComboBox;
-            handle = !cmb.IsDropDownOpen;
-            Handle();
+            //ComboBox cmb = sender as ComboBox;
+            //handle = !cmb.IsDropDownOpen;
+            //Handle();
         }
+
+        private void ClearFilter(object sender, RoutedEventArgs e)
+        {
+            var flowerGet = new BloomingTreesContext();
+            flowerGet.ImagesFlowers.Load();
+            FlowersDG.ItemsSource = flowerGet.ImagesFlowers.Local.ToObservableCollection();
+            uxQueryProductId.Text = null;
+            uxQueryProductId.IsEnabled = false;
+            uxQueryItemName.Text = null;
+            uxQueryItemName.IsEnabled = false;
+            uxQueryItemDescription.Text = null;
+            uxQueryItemDescription.IsEnabled = false;
+            uxQueryItemQty.Text = null;
+            uxQueryItemQty.IsEnabled = false;
+            uxQueryItemSupplier.Text = null;
+            uxQueryItemSupplier.IsEnabled = false;
+            uxQueryPurchaseDate.Text = null;
+            queryDatePicker.IsEnabled = false;
+            uxQueryMessage.Text = null;
+        }
+
+        
 
         private void Handle()
         {
             if (CBSelectQueryField.SelectedItem != null)
-            { //Do Somthing
-
-                string comboBoxSelection = this.CBSelectQueryField.SelectionBoxItem.ToString();
+            { 
+                string comboBoxSelection = CBSelectQueryField.Text.ToString();
                 switch (comboBoxSelection)
                 {
 
                     case "ProductID":
                         {
-                            ProductID.IsEnabled = true;
-                            MessageBox.Show(comboBoxSelection);
+                            uxQueryProductId.IsEnabled = true;
+                            //else disabling all 
+                            uxQueryItemName.IsEnabled = false;
+                            uxQueryItemDescription.IsEnabled = false;
+                            uxQueryItemQty.IsEnabled = false;
+                            uxQueryItemSupplier.IsEnabled = false;
+                            uxQueryPurchaseDate.IsEnabled = false;
+                            selection = "ProductID";
+                            queryDatePicker.IsEnabled = false;
                             break;
                         }
 
                     case "ItemName":
                         {
-                            ItemName.IsEnabled = true;
-                            MessageBox.Show(comboBoxSelection);
+                            uxQueryItemName.IsEnabled = true;
+                            //else disabling all 
+                            uxQueryProductId.IsEnabled = false;
+                            queryDatePicker.IsEnabled = false;
+                            uxQueryItemDescription.IsEnabled = false;
+                            uxQueryItemQty.IsEnabled = false;
+                            uxQueryItemSupplier.IsEnabled = false;
+                            uxQueryPurchaseDate.IsEnabled = false;
                             break;
                         }
 
                     case "Description":
                         {
+                            uxQueryItemDescription.IsEnabled = true;
+                            //else disabling all
+                            uxQueryItemName.IsEnabled = false;
+                            uxQueryProductId.IsEnabled = false;                            
+                            queryDatePicker.IsEnabled = false;
+                            uxQueryItemQty.IsEnabled = false;
+                            uxQueryItemSupplier.IsEnabled = false;
+                            uxQueryPurchaseDate.IsEnabled = false;
                             break;
                         }
 
                     case "Quantity":
                         {
+                            uxQueryItemQty.IsEnabled = true;
+                            //else disabling all 
+                            uxQueryItemDescription.IsEnabled = false;
+                            uxQueryItemName.IsEnabled = false;
+                            uxQueryProductId.IsEnabled = false;                            
+                            queryDatePicker.IsEnabled = false;
+                            uxQueryItemSupplier.IsEnabled = false;
+                            uxQueryPurchaseDate.IsEnabled = false;
                             break;
                         }
 
 
                     case "Supplier":
                         {
+                            uxQueryItemSupplier.IsEnabled = true;
+                            //else disabling all 
+                            uxQueryItemQty.IsEnabled = false;
+                            uxQueryItemDescription.IsEnabled = false;
+                            uxQueryItemName.IsEnabled = false;
+                            uxQueryProductId.IsEnabled = false;                            
+                            queryDatePicker.IsEnabled = false;
+                            uxQueryPurchaseDate.IsEnabled = false;
                             break;
                         }
 
-                    case "Date":
+                    case "Purchase Date":
                         {
+                            queryDatePicker.IsEnabled = true;
+                            uxQueryPurchaseDate.IsEnabled = true;
+                            //else disabling all 
+                            uxQueryItemSupplier.IsEnabled = false;
+                            uxQueryItemQty.IsEnabled = false;
+                            uxQueryItemDescription.IsEnabled = false;
+                            uxQueryItemName.IsEnabled = false;
+                            uxQueryProductId.IsEnabled = false;                     
                             break;
                         }
+
                     default:
                         break;
 
                 }
-
             }
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-        //    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    //string comboBoxSelection;
-        //    //Convert.ToString(comboBoxSelection);         
-        //}
 
         private void selectImageButton_Click(object sender, RoutedEventArgs e)
         {
-
-            //following code would open LoadImageWindow Class but never got it working to update uxAddImagePath with file path
-            //var window = new LoadImageWindow();
-            //Application.Current.MainWindow = window;
-            ////Close();
-            //window.Show();
-
             OpenFileDialog open = new OpenFileDialog();
             open.Title = "Select a picture";
             open.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
               "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
-              "Portable Network Graphic (*.png)|*.png";            
+              "Portable Network Graphic (*.png)|*.png";
             if (open.ShowDialog() == true)
             {
                 displayimg = open.SafeFileName;
@@ -226,7 +389,7 @@ namespace BLOOMINGTREES_WpfApp
                 location = open.FileName;
             }
         }
-        
+
         private void editImageButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
@@ -249,3 +412,4 @@ namespace BLOOMINGTREES_WpfApp
         }
     }
 }
+
